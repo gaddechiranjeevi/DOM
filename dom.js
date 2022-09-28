9c89b5493b31d165ebd766d2835b3081cb1c9797
gaddechiranjeevi <gaddechiru@gmail.com>
2022-09-28 07:09:36 +0530

    first DOM

---
+++ dom.js
@@ -0,0 +1,11 @@
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+
+header.style.borderBottom ='solid 3px #000';
+headerTitle.style.borderBottom='solid 3px black';
+headerTitle.style.borderTop='solid 3px black';
+headerTitle.style.borderLeft='solid 3px black';
+headerTitle.style.borderRight='solid 3px black';
+var main= document.getElementsByClassName('title');
+main[0].fontweight = 'bold';
+main[0].style.color='green';
